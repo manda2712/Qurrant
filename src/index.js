825250b575a0ac52403f18ff7f19d99d3edfc405
Amandaaa <manda27122003@gmail.com>
2025-03-16 15:58:20 +0800

    add new vercel config

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
-const adminAuthorization = require("./middleware/adminAuthorization");
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
 
 const authController = require("./auth/auth.controller")
@@ -24,4 +19,6 @@ app.use("/api/user", userController)
 const readingController = require("./readingProgress/readingProgress.controller")
 app.use("/api/reading", readingController)
 
-// module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
