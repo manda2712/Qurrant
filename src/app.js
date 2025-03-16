b07728895e12c4a9152a23312fcaa730698ed9d0
Amandaaa <manda27122003@gmail.com>
2025-03-16 16:56:21 +0800

    deploy to vercel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 const cors = require('cors')
 
 app.use(express.json());
@@ -19,8 +19,6 @@ app.use("/api/user", userController)
 const readingController = require("./readingProgress/readingProgress.controller")
 app.use("/api/reading", readingController)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
-// export default app;
+
+export default app;
